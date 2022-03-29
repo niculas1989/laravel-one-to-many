619bac0599f0f487ef6a2c5c2062a980b1ef2dea
niculas1989 <nicolas.maranzano@libero.it>
2022-03-29 14:37:23 +0100

    added new info on my DB

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -11,6 +12,19 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+            ['label' => 'PS4', 'color' => 'info'],
+            ['label' => 'XBOX 360', 'color' => 'success'],
+            ['label' => 'XBOX S', 'color' => 'warning'],
+            ['label' => 'Nintendo-Switch', 'color' => 'danger'],
+            ['label' => 'PSP', 'color' => 'secondary']
+        ];
+
+        foreach ($categories as $category) {
+            $c = new Category();
+            $c->label = $category['label'];
+            $c->color = $category['color'];
+            $c->save();
+        }
     }
 }
